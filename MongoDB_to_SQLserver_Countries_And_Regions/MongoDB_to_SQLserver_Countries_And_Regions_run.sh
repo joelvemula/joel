048888335e45870a4421f126c63c2eaa674d87ed
joelvemula <55953469+joelvemula@users.noreply.github.com>
2021-07-30 05:40:34 -0500

    Add files via upload

---
+++ MongoDB_to_SQLserver_Countries_And_Regions/MongoDB_to_SQLserver_Countries_And_Regions_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms256M -Xmx1024M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-slf4j-impl-2.12.1.jar:$ROOT_PATH/../lib/log4j-api-2.12.1.jar:$ROOT_PATH/../lib/log4j-core-2.12.1.jar:$ROOT_PATH/../lib/mongo-java-driver-3.12.0.jar:$ROOT_PATH/../lib/postgresql-42.2.9.jar:$ROOT_PATH/../lib/crypto-utils.jar:$ROOT_PATH/../lib/slf4j-api-1.7.25.jar:$ROOT_PATH/../lib/dom4j-2.1.1.jar:$ROOT_PATH/mongodb_to_sqlserver_countries_and_regions_0_1.jar: local_project.mongodb_to_sqlserver_countries_and_regions_0_1.MongoDB_to_SQLserver_Countries_And_Regions  --context=prod "$@"
