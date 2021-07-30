ee8496515a080545309d6bb5f2b9f50dce974d87
joelvemula <55953469+joelvemula@users.noreply.github.com>
2021-07-30 07:02:47 -0500

    Delete MongoDB_to_SQLserver_Countries_And_Regions directory

--- MongoDB_to_SQLserver_Countries_And_Regions/MongoDB_to_SQLserver_Countries_And_Regions_run.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/mongo-java-driver-3.12.0.jar;../lib/postgresql-42.2.9.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;mongodb_to_sqlserver_countries_and_regions_0_1.jar;' local_project.mongodb_to_sqlserver_countries_and_regions_0_1.MongoDB_to_SQLserver_Countries_And_Regions  --context=prod $args
